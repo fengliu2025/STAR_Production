409f47c5ff55408c450f459bbb6245097079c933
Feng Liu <feng.liu.2@stonybrook.edu>
2026-09-18 14:29:54 -0400

    fix

--- StROOT_ProductionCode_wTrk_Xi_Lambda/StRoot/StPicoHFMaker/StHFPair.h
+++ StROOT_ProductionCode_wTrk_Xi_Lambda/StRoot/StPicoHFMaker/StHFPair.h
@@ -81,8 +81,8 @@ class StHFPair : public TObject
   float pz() const;
   float DcaToPrimaryVertex() const;
 
-  void SetmPointingAngle(float newPointingAngle); // Feng Liu 9/18/2026
-  void SetmDecayLength(float newDecayLength);     // Feng Liu 9/18/2026
+  void SetmPointingAngle(float newPointingAngle){mPointingAngle =  newPointingAngle;} // Feng Liu 9/18/2026
+  void SetmDecayLength(float newDecayLength){mDecayLength = newDecayLength;}     // Feng Liu 9/18/2026
 
  private:
   StHFPair(StHFPair const &);
@@ -131,7 +131,5 @@ inline float StHFPair::v0x() const { return mDecayVertex.x();}
 inline float StHFPair::v0y() const { return mDecayVertex.y();}
 inline float StHFPair::v0z() const { return mDecayVertex.z();}
 inline float StHFPair::DcaToPrimaryVertex() const { return mDcaToPrimaryVertex; }
-inline void SetmPointingAngle(float newPointingAngle) {mPointingAngle =  newPointingAngle} ;
-inline void SetmDecayLength(float newDecayLength) {mDecayLength = newDecayLength};
 #endif
 
